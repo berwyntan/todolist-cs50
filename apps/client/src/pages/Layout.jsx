1cb78297f7452fb46a6827f7b56cf6ddc0af6641
Berwyn Tan <berwyntan@gmail.com>
2023-01-08 21:35:57 +0800

    add loading spinner to refresh

--- apps/client/src/pages/Layout.jsx
+++ apps/client/src/pages/Layout.jsx
@@ -2,22 +2,27 @@ import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { apiRefresh } from "../api/user";
 import { AppContext } from "../App";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
+import Loading from "../components/Loading";
 
 const Layout = () => {
 
   const { setAuthDetails } = useContext(AppContext) || {}
   const navigate = useNavigate()
+  const [ isLoading, setIsLoading ] = useState(false)
 
   const useRefresh = () => {
+    setIsLoading(true)
     apiRefresh()
     .then((response) => {
       if (response?.data?.accessToken) {
         setAuthDetails(response?.data)
         navigate('/')
+        setIsLoading(false)
       }
       else {
         navigate('/login')
+        setIsLoading(false)
       }
     })
   }
@@ -27,7 +32,11 @@ const Layout = () => {
   return (
     <>
         <Navbar />
-        <Outlet />        
+        {
+          isLoading ? 
+          <div className="flex justify-center"><Loading /></div> : 
+          <Outlet />
+        }    
     </>
     
   )
